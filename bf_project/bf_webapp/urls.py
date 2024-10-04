bce7c3024082e98b6be688064ca41881c4908d25
none-type <SonsuzEvren0112358@protonmail.com>
2024-10-04 13:28:19 +0100

    experimenting with grid

--- bf_project/bf_webapp/urls.py
+++ bf_project/bf_webapp/urls.py
@@ -1,6 +1,13 @@
+
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-	path('', index, name='index'),
+    path('', views.index, name='index'),  # Homepage
+    path('about/', views.about, name='about'),
+    path('testimonials/', views.testimonials, name='testimonials'),
+    path('ethos/', views.ethos, name='ethos'),
+    path('key-skills/', views.key_skills, name='key_skills'),
+    path('services/', views.services, name='services'),
+    path('blog/', views.blog, name='blog'),
 ]
--- bf_project/bf_webapp/views.py
+++ bf_project/bf_webapp/views.py
@@ -1,4 +1,24 @@
+# myapp/views.py
+
 from django.shortcuts import render
 
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')  # Specify app namespace
+
+def about(request):
+    return render(request, 'about.html')
+
+def testimonials(request):
+    return render(request, 'testimonials.html')
+
+def ethos(request):
+    return render(request, 'ethos.html')
+
+def key_skills(request):
+    return render(request, 'key_skills.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def blog(request):
+    return render(request, 'blog.html')
