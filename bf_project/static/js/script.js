49651a47bca8d52933423fffad21d1266636da4b
none-type <SonsuzEvren0112358@protonmail.com>
2024-10-14 17:26:29 +0100

    too many changes

---
+++ bf_project/static/js/script.js
@@ -0,0 +1,7 @@
+console.log('JavaScript file is linked properly!');
+
+function toggleMenu() {
+    const navbarLinks = document.getElementById('navbar-links');
+    navbarLinks.classList.toggle('active');
+}
+
